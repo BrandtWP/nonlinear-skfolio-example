09fd51acbf5a29eafcf1714aed3ec69e6672b6ec
Brandt Winkler Prins <48268950+BrandtWP@users.noreply.github.com>
2025-11-29 00:53:21 +0100

    Added curve CV

--- quantlib_adapter.py
+++ quantlib_adapter.py
@@ -4,6 +4,7 @@ from typing import Dict
 import datetime as dt
 import pandas as pd
 
+
 def parse_ql_date(date_str):
     if date_str is None:
         return None
@@ -15,24 +16,35 @@ def parse_ql_date(date_str):
         return ql.Date(date_str.day, date_str.month, date_str.year)
     else:
         raise ValueError(f"Unrecognized date type {type(date_str)}")
-    
-ql_date_to_dt_date = lambda ql_date: dt.date(ql_date.year(), ql_date.month(), ql_date.dayOfMonth())
+
+
+ql_date_to_dt_date = lambda ql_date: dt.date(
+    ql_date.year(), ql_date.month(), ql_date.dayOfMonth()
+)
+
 
 class QLMarketContext(MarketContext):
     quotes: Dict[str, ql.SimpleQuote] = {}
-    relinkable_handles: Dict[str,  "ql.RelinkableHandle"] = {}
+    relinkable_handles: Dict[str, "ql.RelinkableHandle"] = {}
 
-    def __init__(self, date=None, ql_env: Dict[str, "ql.QuoteHandle | ql.RelinkableHandle"] | None = None,
-                 **kwargs):
+    def __init__(
+        self,
+        date=None,
+        ql_env: Dict[str, "ql.QuoteHandle | ql.RelinkableHandle"] | None = None,
+        **kwargs,
+    ):
         super().__init__(date, **kwargs)
 
-        for k, v in ql_env.items():
-            if isinstance(v, ql.SimpleQuote):
-                self.quotes[k] = v
-            elif getattr(v, "linkTo", False):
-                self.relinkable_handles[k] = v
-            else:
-                raise ValueError(f"Unsupported QuantLib object for key '{k}': {type(v)}")
+        if ql_env:
+            for k, v in ql_env.items():
+                if isinstance(v, ql.SimpleQuote):
+                    self.quotes[k] = v
+                elif getattr(v, "linkTo", False):
+                    self.relinkable_handles[k] = v
+                else:
+                    raise ValueError(
+                        f"Unsupported QuantLib object for key '{k}': {type(v)}"
+                    )
 
     def update_eval_date(self):
         ql_date = ql.Date(self.date.day, self.date.month, self.date.year)
@@ -58,20 +70,37 @@ class QLMarketContext(MarketContext):
         # However, we must implement __sklearn_clone__ to conform to the SciKit learn API
         new_ql_env = {}
         for k, v in self.quotes.items():
-            new_ql_env[k] = v  # SimpleQuotes are mutable but we assume user handles cloning if needed
+            new_ql_env[k] = (
+                v  # SimpleQuotes are mutable but we assume user handles cloning if needed
+            )
         for k, v in self.relinkable_handles.items():
-            new_ql_env[k] = v  # RelinkableHandles are mutable but we assume user handles cloning if needed
-        return QLMarketContext(
-            date=self.date,
-            ql_env=new_ql_env,
-            **self.data
-        )
+            new_ql_env[k] = (
+                v  # RelinkableHandles are mutable but we assume user handles cloning if needed
+            )
+        return QLMarketContext(date=self.date, ql_env=new_ql_env, **self.data)
+
 
 class QLInstrumentAdapter(InstrumentAdapter):
     def price(self, market_context: QLMarketContext) -> float:
         market_context.update_ql_env()
         return self.instrument.NPV()
-    
+
+    def cashflow(self, market_context: QLMarketContext) -> float:
+        eval_date = parse_ql_date(market_context.date)
+        if hasattr(self.instrument, "cashflows"):
+            cashflows = self.instrument.cashflows()
+            cashflow_amount = 0.0
+
+            for cashflow in cashflows:
+                if cashflow.date() == eval_date:
+                    cashflow_amount += cashflow.amount()
+
+            if eval_date == self.instrument.maturityDate():
+                cashflow_amount -= self.instrument.redemption().amount()
+
+            return cashflow_amount
+        return 0.0
+
     def __sklearn_clone__(self):
         # Unfortunately, QuantLib objects are not cloneable
         # However, we must implement __sklearn_clone__ to conform to the SciKit learn API
